1e6d9af473e00e1c2797f597f977d5644104906e
Leyder Ceron <leiderlcm8@gmail.com>
2023-12-09 12:10:32 -0500

    Update Prestamo.java

--- src/main/java/com/mycompany/parcial/laboratorio/Prestamo.java
+++ src/main/java/com/mycompany/parcial/laboratorio/Prestamo.java
@@ -1,23 +1,64 @@
 
-package com.mycompany.parcial.laboratorio;
 
-/**
- *
- * @author Leider Ceron
- */
+package taller.mavenproject4;
+
+
 public class Prestamo {
-    public int fechaInicio,fechaFin;
-    Multa multa;
-    public Prestamo(int fechaInicio, int fechaFin) {
+ 
+    //NO PARECE PERO EN TEORIA EL ID DEBE ESTAR PARA ESTO TIPO DE CASOS
+    int id, fechaInicio, fechaFin;
+    Copia copia;
+    Lector lector;
+
+    public Prestamo(int id, int fechaInicio, int fechaFin, Copia copia, Lector lector) {
+        this.id = id;
         this.fechaInicio = fechaInicio;
         this.fechaFin = fechaFin;
-        
+        this.copia = copia;
+        this.lector = lector;
     }
-    public void calcularFechaFin(){
-        
+
+    public int getId() {
+        return id;
     }
-    public void generarMulta(){
-        
-        
+
+    public void setId(int id) {
+        this.id = id;
     }
+
+    public int getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(int fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public int getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(int fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Copia getCopia() {
+        return copia;
+    }
+
+    public void setCopia(Copia copia) {
+        this.copia = copia;
+    }
+
+    public Lector getLector() {
+        return lector;
+    }
+
+    public void setLector(Lector lector) {
+        this.lector = lector;
+    }
+    
+    
+    
+    
 }
