b41f243106f1cdeb143f83b95d64a810c40e805e
Leyder Ceron <leiderlcm8@gmail.com>
2023-12-09 12:08:15 -0500

    Update Libro.java

--- src/main/java/com/mycompany/parcial/laboratorio/Libro.java
+++ src/main/java/com/mycompany/parcial/laboratorio/Libro.java
@@ -1,19 +1,56 @@
 
-package com.mycompany.parcial.laboratorio;
 
-/**
- *
- * @author Leider Ceron
- */
+
+package taller.mavenproject4;
+
+import java.util.LinkedList;
+
+
 public class Libro {
-    public String nombre, tipo,editorial;
-    public int anio;
+    String nombre, tipo, editorial;
+    int anio;
+    LinkedList<Autor> autores = new LinkedList<>();
 
-    public Libro(String nombre, String tipo, String editorial, int anio) {
+    public Libro(String nombre, String tipo, String editorial, int anio, LinkedList<Autor> autores) {
         this.nombre = nombre;
         this.tipo = tipo;
         this.editorial = editorial;
         this.anio = anio;
+        this.autores = autores;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+    
     
 }
