c0d6abe3d7228a366e9f4b90e337b5e79f541951
Leyder Ceron <leiderlcm8@gmail.com>
2023-12-09 12:05:16 -0500

    Update Autor.java

--- src/main/java/com/mycompany/parcial/laboratorio/Autor.java
+++ src/main/java/com/mycompany/parcial/laboratorio/Autor.java
@@ -1,18 +1,44 @@
 
-package com.mycompany.parcial.laboratorio;
 
-/**
- *
- * @author Leider Ceron
- */
+package taller.mavenproject4;
+
+import java.util.LinkedList;
+
+
 public class Autor {
-    public String nombre,nacionalidad;
-    public int fechaNacimiento;
 
-    public Autor(String nombre, String nacionalidad, int fechaNacimiento) {
+    String nombre, nacionalidad, hbd;
+    LinkedList<Libro> libros = new LinkedList<>();
+
+    public Autor(String nombre, String nacionalidad, String hbd, LinkedList<Libro> libros) {
         this.nombre = nombre;
         this.nacionalidad = nacionalidad;
-        this.fechaNacimiento = fechaNacimiento;
+        this.hbd = hbd;
+        this.libros = libros;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getHbd() {
+        return hbd;
+    }
+
+    public void setHbd(String hbd) {
+        this.hbd = hbd;
     }
     
 }
