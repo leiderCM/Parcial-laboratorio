17497b71c6ff0107ab2555d241f8b7faff99d637
Leyder Ceron <leiderlcm8@gmail.com>
2023-12-09 12:09:57 -0500

    Update Multa.java

--- src/main/java/com/mycompany/parcial/laboratorio/Multa.java
+++ src/main/java/com/mycompany/parcial/laboratorio/Multa.java
@@ -1,21 +1,73 @@
 
-package com.mycompany.parcial.laboratorio;
+package taller.mavenproject4;
 
-/**
- *
- * @author Leider Ceron
- */
+
 public class Multa {
-    public int fechaInicio,fechaFin;
-    Prestamo prestamo;
+    int id, fechaInicio, fechaFin;
     Lector lector;
-    public Multa(int fechaInicio, int fechaFin,Lector lector,Prestamo prestamo) {
+    Prestamo prestamo;
+     private double valorAPagar;
+    
+
+    public Multa(int id, int fechaInicio, int fechaFin, Lector lector, Prestamo prestamo) {
+        this.id = id;
         this.fechaInicio = fechaInicio;
         this.fechaFin = fechaFin;
-        this.lector= lector;
-        this.prestamo= prestamo;
+        this.lector = lector;
+        this.prestamo = prestamo;
+       
     }
-    public void calcularFechaFin(){
-        
+
+     
+
+    public void setValorAPagar(double valorAPagar) {
+        this.valorAPagar = valorAPagar;
     }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(int fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public int getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(int fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Lector getLector() {
+        return lector;
+    }
+
+    public void setLector(Lector lector) {
+        this.lector = lector;
+    }
+
+    public Prestamo getPrestamo() {
+        return prestamo;
+    }
+
+    public void setPrestamo(Prestamo prestamo) {
+        this.prestamo = prestamo;
+    }
+
+    String getValorAPagar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
 }
