d42dc591bb17771bb45dae6cb8ad223540a52e5c
Leyder Ceron <leiderlcm8@gmail.com>
2023-12-09 12:06:39 -0500

    Update Copia.java

--- src/main/java/com/mycompany/parcial/laboratorio/Copia.java
+++ src/main/java/com/mycompany/parcial/laboratorio/Copia.java
@@ -1,25 +1,30 @@
 
-package com.mycompany.parcial.laboratorio;
 
-/**
- *
- * @author Leider Ceron
- */
-public class Copia {
-    public int identificador;
-    public String estado;
 
-    public Copia(int identificador, String estado) {
-        this.identificador = identificador;
+package taller.mavenproject4;
+
+import java.util.LinkedList;
+
+
+public class Copia extends Libro {
+    
+    
+    
+    int id;
+    String estado;
+
+    public Copia(int id, String estado, String nombre, String tipo, String editorial, int anio, LinkedList<Autor> autores) {
+        super(nombre, tipo, editorial, anio, autores);
+        this.id = id;
         this.estado = estado;
     }
 
-    public int getIdentificador() {
-        return identificador;
+    public int getId() {
+        return id;
     }
 
-    public void setIdentificador(int identificador) {
-        this.identificador = identificador;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getEstado() {
@@ -29,11 +34,54 @@ public class Copia {
     public void setEstado(String estado) {
         this.estado = estado;
     }
-    
-    public void devolver(){
-        
+
+    @Override
+    public String getNombre() {
+        return nombre;
     }
-    public void prestar(){
-        
+
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
+
+    @Override
+    public String getTipo() {
+        return tipo;
+    }
+
+    @Override
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String getEditorial() {
+        return editorial;
+    }
+
+    @Override
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    @Override
+    public int getAnio() {
+        return anio;
+    }
+
+    @Override
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public LinkedList<Autor> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(LinkedList<Autor> autores) {
+        this.autores = autores;
+    }
+
+   
 }
